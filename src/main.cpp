7a805bf10a0fdc4f78235220859c6519dec22488
Noah Cohn <noahc42@gmail.com>
2023-07-24 02:39:41 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,68 @@
 #include "../include/main.hpp"
 
 
+EM_BOOL gamepad_callback(int eventType, const EmscriptenGamepadEvent *e, void *userData)
+{
+  printf("%s: timeStamp: %g, connected: %d, index: %ld, numAxes: %d, numButtons: %d, id: \"%s\", mapping: \"%s\"\n",
+    eventType != 0 ? emscripten_event_type_to_string(eventType) : "Gamepad state", e->timestamp, e->connected, e->index, 
+    e->numAxes, e->numButtons, e->id, e->mapping);
+
+  if (e->connected)
+  {
+    for(int i = 0; i < e->numAxes; ++i)
+      printf("Axis %d: %g\n", i, e->axis[i]);
+
+    for(int i = 0; i < e->numButtons; ++i)
+      printf("Button %d: Digital: %d, Analog: %g\n", i, e->digitalButton[i], e->analogButton[i]);
+  }
+
+  return 0;
+}
+
+EmscriptenGamepadEvent prevState[32];
+int prevNumGamepads = 0;
+
+void mainloop()
+{
+  EMSCRIPTEN_RESULT res = emscripten_sample_gamepad_data();
+  if (res != EMSCRIPTEN_RESULT_SUCCESS)
+  {
+    printf("emscripten_sample_gamepad_data returned EMSCRIPTEN_RESULT_NOT_SUPPORTED.\n");
+    emscripten_cancel_main_loop();
+    return;
+  }
+
+  int numGamepads = emscripten_get_num_gamepads();
+  if (numGamepads != prevNumGamepads)
+  {
+    printf("Number of connected gamepads: %d\n", numGamepads);
+    prevNumGamepads = numGamepads;
+  }
+
+  for(int i = 0; i < numGamepads && i < 32; ++i)
+  {
+    EmscriptenGamepadEvent ge;
+    int ret = emscripten_get_gamepad_status(i, &ge);
+    if (ret == EMSCRIPTEN_RESULT_SUCCESS)
+    {
+      int g = ge.index;
+      for(int j = 0; j < ge.numAxes; ++j)
+      {
+        if (ge.axis[j] != prevState[g].axis[j])
+          printf("Gamepad %d, axis %d: %g\n", g, j, ge.axis[j]);
+      }
+
+      for(int j = 0; j < ge.numButtons; ++j)
+      {
+        if (ge.analogButton[j] != prevState[g].analogButton[j] || ge.digitalButton[j] != prevState[g].digitalButton[j])
+          printf("Gamepad %d, button %d: Digital: %d, Analog: %g\n", g, j, ge.digitalButton[j], ge.analogButton[j]);
+      }
+      prevState[g] = ge;
+    }
+  }
+}
+
+
 EM_JS(void,js_main,(),{
 
 "use strict";
@@ -51,9 +113,11 @@ slt=tem.innerHTML;
 },8);
 },16)
 ;});
+
 document.getElementById('startBtn').addEventListener('click',function(){
 normalResStart();
 });
+
 setTimeout(function(){
 document.getElementById('circle').width=window.innerWidth;
 document.getElementById('circle').height=window.innerHeight;
@@ -63,6 +127,11 @@ document.getElementById('di').click();
 });
 
 int main(void){
+EMSCRIPTEN_RESULT ret = emscripten_set_gamepadconnected_callback(0, 1, gamepad_callback);
+ret = emscripten_set_gamepaddisconnected_callback(0, 1, gamepad_callback);
+
+  emscripten_set_main_loop(mainloop, 10, 0);
+  
 js_main();
 return 0;
 }
