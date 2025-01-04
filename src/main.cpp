8db5e11496edc89999368eee0c9ab7f68b4f44e9
Noah Cohn <noahc42@gmail.com>
2025-01-04 14:36:05 -0800

    Update main.cpp88

--- src/main.cpp
+++ src/main.cpp
@@ -51,10 +51,10 @@ void mainloop()
 
         //  ADD AXIS
         EM_ASM({
-          if ($0==0){
+          if ($0===0){
           document.querySelector('#axis0').innerHTML=$1;
           }
-          if ($1==1){
+          if ($1===1){
           document.querySelector('#axis1').innerHTML=$1;
           }
           }, j, ge.axis[j]);
@@ -68,28 +68,28 @@ void mainloop()
 
         //  ADD BUTTON
           EM_ASM({
-          if ($0==0){
+          if ($0===0){
           document.querySelector('#button0').innerHTML=$1;
           }
-          if ($1==1){
+          if ($1===1){
           document.querySelector('#button1').innerHTML=$1;
           }
-          if ($1==1){
+          if ($1===1){
           document.querySelector('#button2').innerHTML=$1;
           }
-          if ($1==1){
+          if ($1===1){
           document.querySelector('#button3').innerHTML=$1;
           }
-          if ($1==1){
+          if ($1===1){
           document.querySelector('#button4').innerHTML=$1;
           }
-          if ($1==1){
+          if ($1===1){
           document.querySelector('#button5').innerHTML=$1;
           }
-          if ($1==1){
+          if ($1===1){
           document.querySelector('#button6').innerHTML=$1;
           }
-          if ($1==1){
+          if ($1==88888=1){
           document.querySelector('#button7').innerHTML=$1;
           }
           }, j, ge.analogButton[j]);
